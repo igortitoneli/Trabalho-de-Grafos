5ef18638dac25a501622b9eef3e6d3b52da788ba
Igor <igor.titoneli@estudante.ufjf.br>
2023-12-09 22:23:13 -0300

    testes

--- script_all.sh
+++ script_all.sh
@@ -1,8 +1,8 @@
 instancias=(
-    # "A-n34-k5" 
-    # "A-n48-k7" 
-    # "B-n34-k5"
-    # "B-n50-k8"
+    "A-n34-k5" 
+    "A-n48-k7" 
+    "B-n34-k5"
+    "B-n50-k8"
     "Golden_1"
     "Golden_17"
     "M-n101-k10"
@@ -14,85 +14,83 @@ instancias=(
 maxIter=30
 
 
-# for instancia in "${instancias[@]}"; do
-#     arquivo="./parte_2/resultados/guloso/$instancia.txt"
+for instancia in "${instancias[@]}"; do
+    arquivo="./parte_2/resultados/guloso/$instancia.txt"
 
-#     if [ -e "$arquivo" ]; then
-#         rm "$arquivo"
-#     fi
-#     echo >> "$arquivo"
+    if [ -e "$arquivo" ]; then
+        rm "$arquivo"
+    fi
+    echo >> "$arquivo"
 
-#     valores=0
-#     total_resultado=0
+    valores=0
+    total_resultado=0
 
-#     for x in $(seq 1 1 $maxIter); do
+    for x in $(seq 1 1 $maxIter); do
 
-#         resultado=$(./execGrupo19 "$instancia" teste.txt 1)
-#         echo "$resultado" >> "$arquivo"
+        resultado=$(./execGrupo19 "$instancia" teste.txt 1)
+        echo "$resultado" >> "$arquivo"
         
-#         total=$(echo "$resultado" | awk '/total -/ {sum += $3} END {printf "%.3f", sum}')
-#         valores=$(awk -v valores="$valores" -v total="$total" 'BEGIN {print valores + total}')
-#         total_resultado=$(awk -v total_resultado="$total_resultado" -v total="$total" 'BEGIN {print total_resultado + total}')
+        total=$(echo "$resultado" | awk '/total -/ {sum += $3} END {printf "%.3f", sum}')
+        valores=$(awk -v valores="$valores" -v total="$total" 'BEGIN {print valores + total}')
+        total_resultado=$(awk -v total_resultado="$total_resultado" -v total="$total" 'BEGIN {print total_resultado + total}')
         
-#         tempo=$(echo "$resultado" | awk '/Tempo em ms:/ {print $4}')
-#         tempo_total=$(awk -v valores="$valores" -v total="$tempo" 'BEGIN {print tempo + total}')
-#         tempo_resultado=$(awk -v tempo_resultado="$tempo_resultado" -v tempo="$tempo" 'BEGIN {print tempo_resultado + tempo}')
+        tempo=$(echo "$resultado" | awk '/Tempo em ms:/ {print $4}')
+        tempo_total=$(awk -v valores="$valores" -v total="$tempo" 'BEGIN {print tempo + total}')
+        tempo_resultado=$(awk -v tempo_resultado="$tempo_resultado" -v tempo="$tempo" 'BEGIN {print tempo_resultado + tempo}')
         
 
-#         echo >> "$arquivo"      
-#     done
+        echo >> "$arquivo"      
+    done
 
-#     media=$(awk -v valores="$valores" 'BEGIN {print valores / '$maxIter'}')
-#     echo "Média total para $instancia: $media" >> "$arquivo"
+    media=$(awk -v valores="$valores" 'BEGIN {print valores / '$maxIter'}')
+    echo "Média total para $instancia: $media" >> "$arquivo"
 
 
-#     tempo_medio=$(awk -v valores="$tempo_resultado" 'BEGIN {print valores / '$maxIter'}')
-#     echo "Tempo medio para $instancia: $tempo_medio" >> "$arquivo"
+    tempo_medio=$(awk -v valores="$tempo_resultado" 'BEGIN {print valores / '$maxIter'}')
+    echo "Tempo medio para $instancia: $tempo_medio" >> "$arquivo"
     
-#     tempo_medio=0
-#     tempo_total=0
-#     tempo_resultado=0
-# done
+    tempo_medio=0
+    tempo_total=0
+    tempo_resultado=0
+done
 
 
-# alphas=(0.05 0.10 0.15 0.30 0.50)
+alphas=(0.05 0.10 0.15 0.30 0.50)
 
-# for instancia in "${instancias[@]}"; 
-# do
-#     for alpha in "${alphas[@]}";
-#     do
+for instancia in "${instancias[@]}"; 
+do
+    for alpha in "${alphas[@]}";
+    do
 
-#         arquivo="./parte_2/resultados/guloso_randomizado/alpha_$alpha/$instancia.txt"
-#         if [ -e "$arquivo" ]; then
-#             rm "$arquivo"
-#         fi
-#         echo >> "$arquivo"
-#         valores=0
-#         total_resultado=0
+        arquivo="./parte_2/resultados/guloso_randomizado/alpha_$alpha/$instancia.txt"
+        if [ -e "$arquivo" ]; then
+            rm "$arquivo"
+        fi
+        echo >> "$arquivo"
+        valores=0
+        total_resultado=0
         
-#         for x in `seq 1 1 $maxIter`;
-#         do  
-#             resultado=$(./execGrupo19 $instancia teste.txt 2 $alpha 1000)
-#             echo "$resultado" >> "$arquivo"
+        for x in `seq 1 1 $maxIter`;
+        do  
+            resultado=$(./execGrupo19 $instancia teste.txt 2 $alpha 1000)
+            echo "$resultado" >> "$arquivo"
 
-#             total=$(echo "$resultado" | awk '/total -/ {sum += $3} END {printf "%.3f", sum}')
-#             valores=$(awk -v valores="$valores" -v total="$total" 'BEGIN {print valores + total}')
-#             total_resultado=$(awk -v total_resultado="$total_resultado" -v total="$total" 'BEGIN {print total_resultado + total}')
+            total=$(echo "$resultado" | awk '/total -/ {sum += $3} END {printf "%.3f", sum}')
+            valores=$(awk -v valores="$valores" -v total="$total" 'BEGIN {print valores + total}')
+            total_resultado=$(awk -v total_resultado="$total_resultado" -v total="$total" 'BEGIN {print total_resultado + total}')
 
-#             tempo=$(echo "$resultado" | awk '/Tempo em ms:/ {print $4}')
-#             tempo_total=$(awk -v valores="$valores" -v total="$tempo" 'BEGIN {print tempo + total}')
-#             tempo_resultado=$(awk -v tempo_resultado="$tempo_resultado" -v tempo="$tempo" 'BEGIN {print tempo_resultado + tempo}')
-        
+            tempo=$(echo "$resultado" | awk '/Tempo em ms:/ {print $4}')
+            tempo_total=$(awk -v valores="$valores" -v total="$tempo" 'BEGIN {print tempo + total}')
+            tempo_resultado=$(awk -v tempo_resultado="$tempo_resultado" -v tempo="$tempo" 'BEGIN {print tempo_resultado + tempo}')
+        done
 
-#         done
+        media=$(awk -v valores="$valores" 'BEGIN {print valores / '$maxIter'}')
+        echo "Média total para $instancia: $media" >> "$arquivo"
 
-#         media=$(awk -v valores="$valores" 'BEGIN {print valores / '$maxIter'}')
-#         echo "Média total para $instancia: $media" >> "$arquivo"
-
-#         tempo_medio=$(awk -v valores="$tempo_resultado" 'BEGIN {print valores / '$maxIter'}')
-#         echo "Tempo medio para $instancia: $tempo_medio" >> "$arquivo"
-#     done
-# done
+        tempo_medio=$(awk -v valores="$tempo_resultado" 'BEGIN {print valores / '$maxIter'}')
+        echo "Tempo medio para $instancia: $tempo_medio" >> "$arquivo"
+    done
+done
 
 for instancia in "${instancias[@]}"; 
 do
